b2f1238730e164075a5f6a0eda9f0c0bfc7cdfe1
ConanShin <conan.shin@sk.com>
2020-03-22 22:38:22 +0900

    background introduced

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="home">
+        <div class="menu" @click="showApps">Apps</div>
+        <div class="menu">Projects</div>
+        <div class="menu">Background</div>
     </div>
 </template>
 
@@ -8,11 +11,25 @@
 
     @Component
     export default class Home extends Vue{
+        showApps () {
+
+        }
     }
 </script>
 
 <style scoped lang="scss">
+    $container-padding-vertical: 100px;
+    $container-padding-horizontal: 50px;
+
     .home {
-        background-image: url("../assets/rainny-background.gif")
+        height: calc(100vh - #{$container-padding-vertical * 2});
+        background-size: cover;
+        background-image: url("../assets/rainny-boomerang.gif");
+        padding: $container-padding-vertical $container-padding-horizontal;
+    }
+
+    .menu {
+        font-weight: bold;
+        text-align: right;
     }
 </style>
